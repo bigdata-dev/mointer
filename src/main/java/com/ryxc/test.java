4160f5373ea84652354b9162fc2c362881583f57
tonye0115 <tonye0115>
2016-06-22 13:00:48 +0800

    提交项目

---
+++ src/main/java/com/ryxc/test.java
@@ -0,0 +1,12 @@
+package com.ryxc;
+
+/**
+ * Created by Administrator on 2015/11/28.
+ */
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+
+    }
+}
